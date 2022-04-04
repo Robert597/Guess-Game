d08f27321a1cef36128f5d675a80b4784433be16
Robert597 <robertseun1@gmail.com>
2022-04-05 00:32:54 +0100

    just updated the server

--- gameServer/server.js
+++ gameServer/server.js
@@ -6,14 +6,17 @@ const bodyParser = require("body-parser");
 const connectDB = require("./Model/dbcon.js");
 const PORT = process.env.PORT || 5500;
 require("dotenv").config();
+const path = require("path");
 
 app.use(cors({origin: "*", credentials: true}));
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 connectDB();
-
-app.use("/", require("./Route/route"));
+app.get("/", (req, res) => {
+    return res. status(201).sendFile(path.join(__dirname, "views", "index.html"));
+})
+app.use("/controllers", require("./Route/route"));
 app.get("*", (req, res) => {
     return res.send("Error, page not found");
  })
